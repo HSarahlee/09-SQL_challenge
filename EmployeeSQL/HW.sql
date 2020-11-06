c942a7e8f09b35939efef8e050aa322b1b8899eb
Hanna Lee <sarah@Hannas-MacBook-Pro.local>
2020-11-06 17:30:52 -0500

    update sql files

--- EmployeeSQL/HW.sql
+++ EmployeeSQL/HW.sql
@@ -1,58 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-CREATE TABLE "Department" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL
-);
-
-CREATE TABLE "Dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
-);
-
-CREATE TABLE "Dept_manager" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
-);
-
-CREATE TABLE "Employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" varchar   NOT NULL,
-    "brith_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "sex" varchar   NOT NULL,
-    "hire_date" date   NOT NULL
-);
-
-CREATE TABLE "Salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
-);
-
-CREATE TABLE "Titles" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL
-);
-
-ALTER TABLE "Department" ADD CONSTRAINT "fk_Department_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_manager" ("dept_no");
-
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Dept_emp" ("emp_no");
-
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Salaries" ("emp_no");
-
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Dept_emp" ("emp_no");
-
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "Employees" ("emp_title_id");
 
 --showing the tables
 select * from "Department"
@@ -61,3 +6,55 @@ select * from "Dept_manager"
 select * from "Employees"
 select * from "Salaries"
 select * from "Titles"
+
+--Q1) list employee number, last name, first name, sex, and salary of each employee
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary  
+from "Employees" e inner join "Salaries" s
+on e.emp_no = s.emp_no
+
+
+--Q2) list first name, last name, and hire date for employees who were hired in 1986
+select first_name, last_name, hire_date from "Employees"
+where hire_date >= '1986-01-01T00:00:00' and hire_date < '1987-01-01T00:00:00'
+
+
+--Q3) list the manager of each departmnet, department number, department name, manager's employee number, last name, first, name
+select dm.dept_no, d.dept_name, de.emp_no, e.last_name, e.first_name from
+"Dept_manager" dm inner join "Department" d on dm.dept_no = d.dept_no 
+inner join "Dept_emp" de on dm.emp_no = de.emp_no 
+inner join "Employees" e on de.emp_no = e.emp_no
+
+
+--Q4) list the department of each employees, employee number, last name, first name, and department name
+select de.emp_no, e.last_name, e.first_name, d.dept_name
+from "Employees" e inner join "Dept_emp" de on e.emp_no = de.emp_no
+inner join "Department" d on d.dept_no = de.dept_no
+
+
+--Q5) list first name, last name, and sex for employees whose first name "Hercules" and last name begin with "B"
+select first_name, last_name, sex from "Employees"
+where first_name = 'Hercules' and last_name like 'B%'
+
+
+--Q6) list all employees in the Sales Deparment, including their employee number, last name, first name, and department name
+select d.dept_name, de.emp_no, e.last_name, e.first_name from "Employees" e 
+inner join "Dept_emp" de on e.emp_no = de.emp_no
+inner join "Department" d on d.dept_no = de.dept_no
+where dept_name = 'Sales'
+
+
+--Q7) list all employees in Sales and Development departments, employee number, last name, first name and department name 
+select d.dept_name, de.emp_no, e.last_name, e.first_name from "Employees" e 
+inner join "Dept_emp" de on e.emp_no = de.emp_no
+inner join "Department" d on d.dept_no = de.dept_no
+where dept_name = 'Sales' or dept_name = 'Development'
+
+
+--Q8) list the frequency count of employee last name in descending order 
+select last_name, count(*) as frequency
+from "Employees" 
+group by last_name 
+order by frequency desc
+
+
+
--- EmployeeSQL/codes_from_ERD.sql
+++ EmployeeSQL/codes_from_ERD.sql
@@ -1,19 +1,28 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 CREATE TABLE "Department" (
     "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL
+    "dept_name" varchar   NOT NULL,
+    CONSTRAINT "pk_Department" PRIMARY KEY (
+        "dept_no","dept_name"
+     )
 );
 
 CREATE TABLE "Dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
+    "dept_no" varchar   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Dept_manager" (
     "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "Employees" (
@@ -23,17 +32,26 @@ CREATE TABLE "Employees" (
     "first_name" varchar   NOT NULL,
     "last_name" varchar   NOT NULL,
     "sex" varchar   NOT NULL,
-    "hire_date" date   NOT NULL
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Titles" (
     "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL
+    "title" varchar   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
 ALTER TABLE "Department" ADD CONSTRAINT "fk_Department_dept_no" FOREIGN KEY("dept_no")
@@ -53,11 +71,3 @@ REFERENCES "Dept_emp" ("emp_no");
 
 ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
 REFERENCES "Employees" ("emp_title_id");
-
---showing the tables
-select * from "Department"
-select * from "Dept_emp"
-select * from "Dept_manager"
-select * from "Employees"
-select * from "Salaries"
-select * from "Titles"
